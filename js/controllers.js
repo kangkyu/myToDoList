0027b493162ef2c721797fbadfc5ad2a5d1e61c9
kangkyu <kangkyu1111@gmail.com>
2015-02-18 16:01:27 -0800

    add controllers and complete the app

---
+++ js/controllers.js
@@ -0,0 +1,47 @@
+angular.module('starter.controllers',[])
+
+.controller('ToDoListCtrl',['$scope', '$ionicModal',
+function($scope, $ionicModal){
+  
+  $scope.toDoListItems = [
+    {
+      task: 'Scuba Diving',
+      status: 'not done'
+    },
+    {
+      task: 'Climb Everest',
+      status: 'not done'
+    }
+  ];
+
+  $ionicModal.fromTemplateUrl('modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  $scope.AddItem = function(data){
+    $scope.toDoListItems.push({
+      task: data.newItem,
+      status: 'not done'
+    });
+    data.newItem = '';
+    $scope.closeModal();
+  };
+}])
+
+
